240f08d46fd9c88ec6a0ff01f1d1128886fd7a7e
Milan Hradil <milan.hradil@meteogroup.com>
2016-03-24 09:46:17 +0100

    Change source for api bluepring file

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
-curl -s "https://raw.githubusercontent.com/MeteoGroup/weather-api/master/WEATHER-API-BLUEPRINT.md" > API.md
+curl -s "https://raw.githubusercontent.com/MeteoGroup/weather-api/master/WEATHER-API-BLUEPRINT.apib" > API.md
 aglio --theme-template ./template/index.jade --theme-style ./template/layout-default.less --theme-style ./template/meteogroup.less -i API.md -o index.html
 rm API.md
